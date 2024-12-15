a5cd6478e4014e6c23cd27af336ba20f75984bcd
Sahil <sahilsingh20@iitk.ac.in>
2024-12-15 22:17:03 +0530

    requirements fix:

--- setup.py
+++ setup.py
@@ -3,23 +3,21 @@ from setuptools import setup, find_packages
 # Read the contents of the README.md file
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
+    
+# Read requirements.txt
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
 
 setup(
     name="comet_labs",  # Your package name
-    version="0.1.5",
+    version="0.1.6",
     description="A CLI tool for AI-driven commit messages and Jira integration",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Sahil",
     author_email="sahil85.10s@gmail.com",
     packages=find_packages(),  # Automatically find all sub-packages
-    install_requires=[
-        "openai",
-        "python-dotenv",
-        "requests",
-        "textblob>=0.17.1",
-        # Add other dependencies here
-    ],
+    install_requires=requirements,
     entry_points={
         "console_scripts": [
             "comet-labs=comet_labs.cli:main",  # Maps 'comet-labs' to the main() function in cli.py
